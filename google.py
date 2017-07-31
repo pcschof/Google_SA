0177dcce47e3a35eda1b2cc6e8db3a577942d2b1
Peter Schofield <pschofield@stansberryresearch.com>
2017-07-31 11:34:57 -0400

    Script to run google sentiment and entity analysis

--- google.py
+++ google.py
@@ -1,18 +1,45 @@
+
+import urllib.request
 from google.cloud import language
 
-def language_analysis(text):
-    client = language.Client()
-    document = client.document_from_text(text)
-    sent_analysis = document.analyze_sentiment()
-    print(dir(sent_analysis))
-    sentiment = sent_analysis.sentiment
-    ent_analysis = document.analyze_entities()
-    entities = ent_analysis.entities
 
-    example_text = 'Is it not obvious that Python is the best programming language of them all'
+class google_sent():
+    # initialize connection to dynamo endpoint
+    url = 'http://ec2-174-129-144-17.compute-1.amazonaws.com:8184/sbcmanager/articles/ROM'
+    # request connection
+    req = urllib.request.Request(url)
+    # open url
+    resp = urllib.request.urlopen(req)
+    # read text in url
+    resp_data = resp.read()
+    # decode in utf-8 format
+    resp_data = resp_data.decode("utf-8")
+    # print results
+    print(resp_data)
+
+
+    def language_analysis(self, text):
+        # establish connection to Google Nat Language API
+        client = language.Client()
+        # initialize content as a document from text
+        document = client.document_from_text(text)
+        # call analyze_sentiment method
+        sent_analysis = document.analyze_sentiment()
+        print(sent_analysis)
+        sentiment = sent_analysis.sentiment
+        ent_analysis = document.analyze_entities()
+        entities = ent_analysis.entities
+        return sentiment, entities
+
+
+if __name__ == '__main__':
+    sent = google_sent()
+    example_text = google_sent().resp_data
+    sentiment, entities = sent.language_analysis(example_text)
     print(sentiment.score, sentiment.magnitude)
-    return sentiment, entities
 
-def print_entites(entities):
     for e in entities:
-        print(e.name, e.entity_type, e.metadata, e.salience)
+       print(e.name, e.entity_type, e.metadata, e.salience)
+
+
+
